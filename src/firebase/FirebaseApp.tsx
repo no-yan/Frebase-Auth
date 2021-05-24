8cce085bad0c1fbfec841c9fdaafd243d54d5141
noyan <1.2.3.up.top.charm00ok001996991@gmail.com>
2021-05-24 18:06:06 +0900

    delete files and omit css

--- src/firebase/FirebaseApp.tsx
+++ src/firebase/FirebaseApp.tsx
@@ -1,20 +1,18 @@
-import initFirebase from "./initFirebase";
+import { FC, useState, useEffect, useRef, useContext } from "react";
 import firebase from "firebase/app";
 import "firebase/auth";
-import { useState, useEffect, useContext, FC, useRef } from "react";
+
 import { FirebaseContext, UserContext } from "../contexts/contexts";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-
-initFirebase();
-
-const auth = firebase.auth();
 
 type Props = { children: JSX.Element };
 const FirebaseApp: FC<Props> = ({ children }) => {
-  const { userName, setUserName } = useContext(UserContext);
+  console.log("rendered", Date.now());
+  const [userName, setUserName] = useState<string | null>(null);
   const counterRef = useRef(0);
+  const auth = firebase.auth();
 
   const unsubscribe = firebase.auth().onAuthStateChanged(function (user) {
+    console.log(user);
     if (counterRef.current === 1 && user) {
       //processing for signing in
       console.log(user.displayName);
